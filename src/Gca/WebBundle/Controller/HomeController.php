355bfee6f254eb0d3b4e133c04bae295f4f16f19
eduardo <eduardo.avilesj@gmail.com>
2011-11-01 20:22:02 -0600

    se agrego el codigo para congtrolar el formulario

--- src/Gca/WebBundle/Controller/HomeController.php
+++ src/Gca/WebBundle/Controller/HomeController.php
@@ -2,6 +2,18 @@
 
 namespace Gca\WebBundle\Controller;
 
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+
+use Gca\WebBundle\Entity\Usuario;
+
+use Gca\WebBundle\GcaWebBundle;
+
+use Symfony\Component\HttpFoundation\Request;
+
+use Gca\WebBundle\Form\UsuarioType;
+
+use Symfony\Component\Security\Core\User\User;
+
 use Symfony\Component\Security\Core\SecurityContext;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -28,8 +40,49 @@ class HomeController extends Controller
 		));
 	}
 
-	public function registerAction() {
-		return $this->render('GcaWebBundle:Home:register.html.twig');
+	public function registerAction(Request $request) {
+		
+		
+//		$usuario = new Usuario();
+//		$form = $this->createForm(new UsuarioType(),$usuario);
+		
+// 		if ($request->getMethod()=='POST') {
+// 			$form->bindRequest($request);
+// 			if ($form->isValid()){
+// 				//Seteamos el mensaje de registro satisfactorio
+// 				$session = $request->getSession();
+// 				$session->setFlash('mensaje','Gracias por registrar sus datos');
+				
+// 				//obtenemos los datos
+// 				$usuario = $form->getData();
+				
+// 				//Codificamos la contraseña
+// 				$factory = $this->get('security.encoder_factory');
+// 				$codificador = $factory->getEncoder($usuario);
+// 				$password = $codificador->encodePassword($usuario->getPassword(), $usuario->getSalt());
+// 				$usuario->setPassword($password);
+				
+// 				//Guardamos el usuario en la base de datos
+// 				$em = $this->getDoctrine()->getEntityManager();
+// 				$em->persist($usuario);
+// 				$em->flush();
+				
+//      			//logueamos al usuario
+     			
+// 				$token = new UsernamePasswordToken($usuario, null, 'main', $usuario->getRoles());
+// 				$this->get('security.context')->setToken($token);
+								
+				
+// 				return $this->render('GcaWebBundle:Home:bienvenido.html.twig',
+// 						array(
+// 								'name'=>$usuario->getNombre()." ".$usuario->getApellido()													
+// 							)
+// 						);
+				
+// 			};
+// 		}
+		
+		return $this->render('GcaWebBundle:Home:register.html.twig'/*,array('form' => $form->createView())*/);
 	}
 	
 	function despedidaAction() {
